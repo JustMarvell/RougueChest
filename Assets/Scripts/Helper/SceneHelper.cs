1d4f20a80c5ce7540e433d9371d76b04b9929b5c
JustMarvell <marvelmakaluwu050405@gmail.com>
2026-07-07 06:04:42 +0800

    add helper tp function

--- Assets/Scripts/Helper/SceneHelper.cs
+++ Assets/Scripts/Helper/SceneHelper.cs
@@ -14,4 +14,39 @@ public class SceneHelper : MonoBehaviour
         Debug.Log("Quit Game");
         Application.Quit();
     }
+
+    public void TPToWhite()
+    {
+        Vector3 position = new Vector3(3.5f, 8f, -3f);
+        Quaternion rotation = Quaternion.Euler(new Vector3(55f, 0f, 0f));
+        Camera.main.transform.SetPositionAndRotation(position, rotation);
+    }
+
+    public void TPToBlack()
+    {
+        Vector3 position = new Vector3(3.5f, 8f, 9f);
+        Quaternion rotation = Quaternion.Euler(new Vector3(55f, -180f, 0f));
+        Camera.main.transform.SetPositionAndRotation(position, rotation);
+    }
+
+    public void TPToTOP()
+    {
+        Vector3 position = new Vector3(3.5f, 10f, 3.5f);
+        Quaternion rotation = Quaternion.Euler(new Vector3(90f, 0f, 0f));
+        Camera.main.transform.SetPositionAndRotation(position, rotation);
+    }
+
+    public void TPToLeft()
+    {
+        Vector3 position = new Vector3(-3f, 8f, 3.5f);
+        Quaternion rotation = Quaternion.Euler(new Vector3(55f, 90f, 0f));
+        Camera.main.transform.SetPositionAndRotation(position, rotation);
+    }
+
+    public void TPToRight()
+    {
+        Vector3 position = new Vector3(9f, 8f, 3.5f);
+        Quaternion rotation = Quaternion.Euler(new Vector3(55f, -90f, 0f));
+        Camera.main.transform.SetPositionAndRotation(position, rotation);
+    }
 }
